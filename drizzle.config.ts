7bba86e7455f3f9a142da13f525da1ab60bdf81b
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2024-10-18 19:25:35 -0300

    feat: initialize API with Bun and Elysia - configure PostgreSQL in Docker Compose - set up Drizzle ORM with initial models and relationships - create migrations for database structure

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./src/db/schema/index.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    host: "localhost",
+    port: 5432,
+    user: "docker",
+    password: "docker",
+    database: "pizzashop",
+  }
+} satisfies Config;
