e5e6443d02c2f4432445a6e892bd05dc5fe4dfe8
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2024-10-19 19:23:42 -0300

    feat: implement authentication link schema, JWT setup, and routes for authentication flow
    - Add schema for authentication links
    - Create route for sending authentication link
    - Setup JWT-based authentication
    - Implement route for authentication via link
    - Add route for logout and session termination

--- src/env.ts
+++ src/env.ts
@@ -1,15 +1,20 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
+  API_BASE_URL: z.string().default('http://localhost:3333'),
+  AUTH_REDIRECT_URL: z.string().default('http://localhost:5173'),
   DB_HOST: z.string().default('localhost'),
   DB_PORT: z.number().default(5432),
   DB_USER: z.string().default('docker'),
   DB_PASSWORD: z.string().default('docker'),
   DB_NAME: z.string().default('pizzashop'),
   DB_SSL: z.boolean().default(false),
+  JWT_SECRET_KEY: z.string().default('yu64dUaHCMFsXh2'),
 })
 
 export const env = envSchema.parse({
+  API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:3333',
+  AUTH_REDIRECT_URL: process.env.API_BASE_URL ||'http://localhost:5173',
   DB_HOST: process.env.DB_HOST || 'localhost',
   DB_PORT: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
   DB_USER: process.env.DB_USER || 'docker',
