4e2281befa27b53b02210429d811e2d968b074d5
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2024-11-30 10:48:40 -0300

    feat: Implement email sending functionality with Nodemailer

--- src/http/routes/send-auth-link.ts
+++ src/http/routes/send-auth-link.ts
@@ -3,6 +3,8 @@ import { db } from '../../db/connection'
 import { createId } from '@paralleldrive/cuid2'
 import { authLinks } from '../../db/schema'
 import { env } from '../../env'
+import nodemailer from 'nodemailer'
+import { mail } from '../../lib/mail'
 
 export const sendAuthLink = new Elysia().post(
   '/authenticate',
@@ -36,7 +38,16 @@ export const sendAuthLink = new Elysia().post(
     authLink.searchParams.set('code', authLinkCode)
     authLink.searchParams.set('redirect', env.AUTH_REDIRECT_URL)
 
-    console.log(authLink.toString())
+    const info = await mail.sendMail({
+      from: {
+        name: 'La Pizzeria',
+        address: 'hi@lapizzeria.com',
+      },
+      to: email,
+      subject: 'Authenticate to La Pizzeria',
+      text: `Use the following link to authenticate on La Pizzeria: ${authLink.toString()}`,
+    })
+    console.log(nodemailer.getTestMessageUrl(info))
   },
   {
     body: t.Object({
